a5c2d9b5f902ab931af791836afb27089071e93a
Adam Vinch <adamvinch@gmail.com>
2023-11-21 17:17:44 -0500

    Add responsive hamburger menu icon that can toggle and becomes hidden on bigger displays

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,43 @@
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 import Logo from '../assets/logo.svg'
+import Menu from '../assets/menu-icon.svg'
+
 import './NavBar.css'
 
 
+
+
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
+//  <Link to="/">
+//    <img className="logo" src={Logo}></img>
+//  </Link>
+
+
+
   return (
     <nav className='nav-container'>
-      <Link to="/">
-        <img className="logo" src={Logo}></img>
-      </Link>
-      <Link className="nav-link" to="/about">About</Link>
-      <Link className="nav-link" to="/projects">Projects</Link>
-      <Link className="nav-link" to="/contact">Contact</Link>
+      <img 
+        className="menu-icon" 
+        src={Menu}
+        onClick={toggleMenu}>
+      </img>
+
+      <ul style={{display: menuOpen ? 'inline':'none'}}>
+        <li><Link className="nav-link" to="/about">About</Link></li>
+        <li><Link className="nav-link" to="/projects">Projects</Link></li>
+        <li><Link className="nav-link" to="/contact">Contact</Link></li>
+      </ul>
+
+
+      
     </nav>
   )
 }
