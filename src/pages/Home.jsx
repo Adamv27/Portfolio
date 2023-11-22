8ce99c9d06bbd8d059a71bcc1bf37e97b56ab680
Adam Vinch <jjavinch@me.com>
2023-11-22 15:21:05 -0500

    Add color to home page text and background image. Center home content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import NavBar from '../components/NavBar'
 import ComputerGraphic from '../assets/computer-image.svg'
-
 import "../styles/Home.css"
 import "../styles/global.css"
 
@@ -11,7 +10,7 @@ const Home = () => {
       <div className='home-content'>
         <div className='text-container'>
           <p className='home-text'>Hi, I am</p>
-          <h1 className='home-title'>Adam Vinch</h1>
+          <h1 className='home-text title'>Adam Vinch</h1>
           <p className='home-text bottom'>Software Developer</p>
           <a className='contact-link'>Contact Me</a>
         </div>
