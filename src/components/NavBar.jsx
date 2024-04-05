f97b8c8047a099ef69f18e9a2d8256817f0777ed
Adam Vinch <adamvinch@gmail.com>
2024-04-05 11:35:12 -0400

    Redesign homepage using mobile first strategy

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,9 +11,6 @@ const NavBar = () => {
   return (
     <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="/">
-          <Image src={Logo} />          
-        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="auto">
