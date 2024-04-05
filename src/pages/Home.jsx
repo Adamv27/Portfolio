933d12015eda892eac1e23f2304cf7e5b291ac86
Adam Vinch <adamvinch@gmail.com>
2024-04-05 00:24:02 -0400

    Add homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
 import ComputerGraphic from '../assets/computer-image.svg'
+
+import '../styles/home.css'
+import '../scss/Home.scss'
 
 const Home = () => {
   return (
-    <Container>
-      <p>Hi, I am</p>
-      <h1>Adam Vinch</h1>
-      <p>Computer Science student and aspiring Software Developer</p>
-      <Button>See my work</Button>    
+    <Container className="home-container d-flex flex-row justify-content-center align-items-center">
+      <div className="content">
+        <p className="home-text">Hi, I am</p>
+        <h1 id="title" className="home-text">Adam Vinch</h1>
+        <p className="home-text">Computer Science student and <br/>aspiring Software Developer</p>
+        <Button id="home-button">See my work</Button>    
+      </div>
+      <div className="content">
+        <Image id="graphic" src={ComputerGraphic} />
+      </div>
     </Container>
   )
 }
