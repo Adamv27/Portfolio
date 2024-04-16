8bc2bf071cc0df402ab75675c33d5641c0fff189
Adam Vinch <adamvinch@gmail.com>
2024-04-16 19:20:38 -0400

    Begin adding projects to projects page

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,39 +1,32 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Stack, Badge } from "react-bootstrap";
 
-import LinkIcon from '../assets/external-link.svg'
-
-
-function ProjectCard() {
-  const tagBackgrounds = {
-    'Python': {backgroundColor: '#E6C75D' },
-    'Java': {backgroundColor: '#B08BBB' },
-    'JavaScript': {backgroundColor: '#F8B88B' },
-    'HTML/CSS': {backgroundColor: '#FD8A8A' },
-    'SQLite3': {backgroundColor: '#73D673'},
-    'React': {backgroundColor: '#B2CEFE'},
-    'Swing': {backgroundColor: '#32CACD'}
-  }
-/*
-    <div className='tags'> 
-          {tags.map(tag => (
-            <span 
-              className='tag' 
-              key={tag}
-              style={tagBackgrounds[tag]}
-            > {tag}</span>
-          ))}
-        </div>
-*/
-
+function ProjectCard({ 
+  title, 
+  description,
+  imageSrc,
+  githubUrl,
+  demoUrl,
+  tags,
+}) {
+  
   return (
     <Card id="project-card" style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="" />
+      <Card.Img variant="top" src={imageSrc} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick exmaple text to build on the card title and make up the bulk of the card's content. 
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Stack direction="horizontal" gap={1}>
+          {tags && tags.map(tag => (
+            <Badge 
+              key={tag} 
+              bg={tag.toLowerCase()}
+            >{tag}</Badge>
+          ))}
+        </Stack>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Button variant="primary" as="a" href={githubUrl}>GitHub</Button>
+        {demoUrl && (
+          <Button>Demo</Button>
+        )}
       </Card.Body>
     </Card>
   )
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,46 @@
+import { Container } from "react-bootstrap"
+
+import ProjectCard from "../components/ProjectCard"
+import Minesweeper from "../assets/projects/minesweeper.png"
+import StockViewer from "../assets/projects/stock-viewer.png"
+import Blackjack from "../assets/projects/blackjack.png"
+import Sentry from "../assets/projects/sentry.png"
+
 const Projects = () => {
   return (
-    <div>
-      Projects
-    </div>
+    <Container>
+      <ProjectCard 
+        title="Card Title"
+        description="Some quick exmaple text to build on the card title and make up the bulk of the card's content."
+        imageSrc={Minesweeper}
+        githubUrl=""
+      />
+      <ProjectCard 
+        title="Sentry"
+        description="A password manager in Java Swing designed with the MVC architecture. Built Apr. 2023"
+        imageSrc={Sentry}
+        githubUrl="https://github.com/Adamv27/Sentry"
+      />
+      <ProjectCard 
+        title="Stock Viewer"
+        description="Stock price viewer using the financial modeling prep API and HTML canvas for a custom price graph. Built Dec. 2021"
+        imageSrc={StockViewer}
+        githubUrl="https://github.com/Adamv27/Stock-Viewer"
+      />
+      <ProjectCard 
+        title="Minesweeper"
+        description="Classic 1989 version of minesweeper made with vanilla JavaScript. Built Dec. 2021"
+        imageSrc={Minesweeper}
+        githubUrl="https://github.com/Adamv27/Minesweeper-JS"
+      />
+      <ProjectCard 
+        title="Blackjack"
+        description="Game of blackjack built using pygame with custom drag and drop mechanics. Built Nov. 2021"
+        imageSrc={Blackjack}
+        githubUrl="https://github.com/Adamv27/Blackjack"
+        tags={["Python", "Java"]}
+      />
+    </Container>
   )
 }
 
