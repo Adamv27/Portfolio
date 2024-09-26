a5dafe7bff1589a4100e8a99158eff962c5df43d
Adamv27 <adamvinch@gmail.com>
2024-09-26 12:46:20 -0400

    Add logo as link to home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,19 @@ import { Link } from "react-router-dom";
 
 
 import "../styles/NavBar.css";
+import logo from "../assets/logo.svg";
 
 const NavBar = () => {
 	return (
 		<nav id="navbar">
-			<Link to="about">About</Link>
-			<Link to="projects">Projects</Link>
-			<Link to="contact">Contact</Link>
+			<Link to="/">
+				<img src={logo} width="50" height="50" />
+			</Link>
+			<div>
+				<Link to="blog">Blog</Link>
+				<Link to="projects">Projects</Link>
+				<Link to="contact">Contact</Link>
+			</div>
 		</nav>
 	)
 
