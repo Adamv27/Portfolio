1c54eb215fe62f3e26a8a8fcfb96bdabe53ea01e
Adamv27 <adamvinch@gmail.com>
2024-09-26 12:46:51 -0400

    Change about to blog page

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
 
 import Layout from './pages/Layout';
 import Home from './pages/Home';
-import About from './pages/About';
+import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
 
@@ -16,7 +16,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route index element={<Home />} />
-      <Route path='about' element={<About />} />
+      <Route path='blog' element={<Blog />} />
       <Route path='projects' element={<Projects />} />
       <Route path='contact' element={<Contact />} />
     </Route>
---
+++ src/pages/Blog.jsx
@@ -0,0 +1,13 @@
+
+
+const Blog = () => {
+	return (
+		<>
+			<p>Coming Soon!</p>
+			<p>I am current working on a backend system with user auth that will allow me to</p>
+			<p>create and upload blog pages here.</p>
+		</>
+	)
+}
+
+export default Blog 
