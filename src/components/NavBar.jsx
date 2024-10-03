6489f55080a365a5a84b6e1935ceb304851ff118
Adamv27 <adamvinch@gmail.com>
2024-10-03 02:16:42 -0400

    Responsive Nav Bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,47 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "../styles/NavBar.css";
 import logo from "../assets/logo.svg";
+import menuIcon from "../assets/menu-icon.svg";
+import closeIcon from "../assets/close-icon.svg"
 import Resume from "../assets/Adam_Vinch_Resume.pdf";
-import NavMenu from "./NavMenu";
 
 const NavBar = () => {
+	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<nav id="navbar">
-			<Link to="/">
-				<img src={logo} width="50" height="50" />
-			</Link>
-			<div id="nav-links">
-				<Link to="projects">Projects</Link>
-				<Link to="contact">Contact</Link>
-				<a href={Resume} id='resume-link' target='_blank' rel='noopener noreferrer'>Resume</a>
+			<div id="navbar-container">
+				<Link to="/">
+					<img id="logo" src={logo} width="50" height="50" />
+				</Link>
+				<div id="nav-links">
+					<Link to="projects">Projects</Link>
+					<Link to="contact">Contact</Link>
+					<a href={Resume} id='resume-link' target='_blank' rel='noopener noreferrer'>Resume</a>
+				</div>
 			</div>
-			<NavMenu />
-		</nav>
+
+			<div id="nav-menu">
+				<img
+					src={menuIcon}
+					className={`nav-menu-icon ${isOpen ? "hide" : "show"}`}
+					onClick={() => setIsOpen(!isOpen)}
+				/>
+				<img
+					src={closeIcon}
+					className={`nav-menu-icon ${isOpen ? "show" : "hide"}`}
+					onClick={() => setIsOpen(!isOpen)}
+				/>
+				<div className={`nav-menu-links ${isOpen ? "show" : "hide"}`}>
+					<Link to="projects">Projects</Link>
+					<Link to="contact">Contact</Link>
+					<a href={Resume} id='resume-link' target='_blank' rel='noopener noreferrer'>Resume</a>
+				</div>
+
+
+			</div>
+		</nav >
 	)
 }
 
