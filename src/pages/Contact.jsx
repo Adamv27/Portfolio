815f8f4da39d8c8d6d13292bae716c14c017b9ba
Adam Vinch <adamvinch@gmail.com>
2024-10-30 15:48:42 -0400

    Add placeholder to message box in contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
 				<legend>Contact Me</legend>
 				<input className="contact-control" name="email" type="email" placeholder="Email Address" />
 				<input className="contact-control" name="name" placeholder="Name" />
-				<textarea className="contact-control" name="message"></textarea>
+				<textarea className="contact-control" name="message" placeholder="Message"></textarea>
 				<input type="hidden" name="access_key" value="61ac1e6d-92b3-45b8-bcfd-5d708040af6c"></input>
 				<button className="contact-control" type="submit">Send Message</button>
 			</form>
